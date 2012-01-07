d99956e55ad873f581e764aa9a7813bb5a21c10c
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2012-01-07 11:56:22 +0100

    Funkcje pomocnicze

--- TGS-III/Ford_Fulkersoncs.cs
+++ TGS-III/Ford_Fulkersoncs.cs
@@ -14,6 +14,9 @@ namespace TGS_III
         //macierz identyczna jak w przypadku matrix z Graph
         Int16?[][] matrix = new Int16?[0][];
 
+        //liczba wierzcholkow - wnioskowana z macierz po jej zaladowaniu
+        int edges_num = -1;
+
         public void Flow(int start, int stop, Int16?[][] _matrix)
         {
             setFlowMatrix(_matrix);
@@ -43,6 +46,24 @@ namespace TGS_III
         /// <param name="_matrix">Nowa macierz przepływu</param>
         private void setFlowMatrix(Int16?[][] _matrix) {
             matrix = _matrix;
+
+            edges_num = findEdgesNum(matrix);
+        }
+
+        /// <summary>
+        /// Określa maksymalną ilość wierzchołków w macierzy matrix
+        /// </summary>
+        /// <param name="matrix">Macierz z danymi nt. przejść w grafie</param>
+        /// <returns>Maksymalna liczba wierzchołków</returns>
+        private int findEdgesNum(short?[][] matrix)
+        {
+            int num = -1;
+            for(int i= 0; i< matrix.Length; i++){
+                if (num < matrix[i].Length) {
+                    num = matrix[i].Length;
+                }
+            }
+            return num;
         }
 
         /// <summary>
@@ -78,7 +99,27 @@ namespace TGS_III
         /// <returns></returns>
         private int findMinFlow(List<int> path)
         {
-            throw new NotImplementedException();
+            int? min = null;
+            int f = -1;
+            for (int i = 0; i < path.Count; i++)
+            {
+                f = flow(path[i],path[i+1]);
+                if ((min == null) || (min > f && f >= 0 )) {
+                    min = f;
+                }
+            }
+
+            return (int)min;
+        }
+
+        private int flow(int start, int end)
+        {
+            foreach (short?[] arr in matrix) {
+                if (arr[end] == -1 && arr[start] > 0) {
+                    return (int)arr[end];   
+                }
+            }
+            throw new ArgumentException("Flow: nie ścieżki pomiędzy " + start + " a " + end);
         }
 
         /// <summary>
@@ -87,10 +128,89 @@ namespace TGS_III
         /// </summary>
         /// <param name="start">Wierzchołek początkowy</param>
         /// <param name="stop">Wierzchołek końcowy</param>
-        /// <returns></returns>
+        /// <returns>Lista int'ów jako kolejnych wierzchołków od przejścia</returns>
         private List<int> findPath(int start, int stop)
         {
-            throw new NotImplementedException();
+            //lista wierzcholkow przez ktore musimy przejsc by dojsc z start do stop
+            //start i stop też są na tej liscie
+            List<int> path = new List<int>();
+            path.Add(start);
+
+            //obecnie wybrany wierzchołek
+            int element = -1;
+
+            int found = -1;
+
+            //ostatni wierzchołek na ścieżce
+            int last = -1;
+
+            while (true) {
+
+                last = path.Count - 1;
+
+                //znajdz wierzcholek polaczony dodatnia droga z ostatnim na sciezce, wiekszy niz element
+                //lub zwroc -1 gdy nie ma takiego
+                found = findWay(path[last], element);
+                
+                //jesli nie ma takiego przejscia to zdejmij wierzcholke z gory i szukaj dalej
+                if (found == -1)
+                {
+                    if(element >= edges_num){
+                        element++;
+                    }else{
+                        //cofnij sie o krok na ścieżce
+                        element = path[last];
+                        path.RemoveAt(last);
+                    }
+                }
+                else {
+                    if (!path.Contains(found))
+                    {
+                        path.Add(last);
+                        element = -1;
+                    }
+
+                    if (path[last] == stop)
+                    {
+                        raport_str.Append(representPath(path));
+                        Console.WriteLine(representPath(path));
+                        return path;
+                    }
+                }
+
+            }
+        }
+
+
+        private String representPath(List<int> path)
+        {
+            StringBuilder sb = new StringBuilder("Scieżka: ");
+
+            foreach(int edge in path){
+                sb.Append(edge);
+                sb.Append("->");
+            }
+            sb.Remove(sb.Length - 2, 2);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Znajduje wierzchołek połączony dodatnią droga z p, większy niż element
+        /// lub zwróć -1 gdy taki nie istnieje
+        /// </summary>
+        /// <param name="p">Numer wierzchołka w którym jesteśmy</param>
+        /// <param name="element">Numer wierzchołka ma być większy niż</param>
+        /// <returns></returns>
+        private int findWay(int p, int element)
+        {
+            for (int i = element + 1; i < matrix.Length; i++) {
+                if (matrix[i][p] != null && matrix[i][p] > 0) {
+                    return (int)matrix[i][p];
+                }
+            }
+
+            return -1;
         }
 
         /// <summary>
