c759feb21f23ef0f02d40893d5b6d102c13ec2e0
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2012-01-14 14:06:46 +0100

    Dokumentacja

---
+++ TGS-III/Edmonds_Karp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TGS_III
+{
+    class Edmonds_Karp : Ford_Fulkerson
+    {
+        //ta wersja metody Forda i Filkersona wmyślona przez Edmondsa i Karpa
+        //polega na szukaniu ścieżek które są najkrótsze,
+        // w sensie mają minimalną ilość krawędzi
+
+    }
+}
